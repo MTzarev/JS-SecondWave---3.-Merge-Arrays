cdccfb0e61e29e16fc936b7b39ddd0c32265dacd
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-25 22:04:35 +0300

    Add files via upload

---
+++ 3. Merge Arrays.js
@@ -0,0 +1,17 @@
+function mergeArrays(arr1, arr2) {
+    newArr = [];
+    for (let i = 0; i < arr1.length; i++) {
+        let num1 = arr1[i];
+        let num2 = arr2[i];
+        if (i % 2 === 0) {
+            newArr.push(Number(num1) + Number(num2));
+        } else {
+            newArr.push(num1.concat(num2));
+        }
+
+    }
+    console.log(newArr.join(` - `));
+
+}
+mergeArrays(['5', '15', '23', '56', '35'],
+    ['17', '22', '87', '36', '11']);
