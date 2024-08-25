a15d3988a7e73727395275f1cb1cfcb721115415
Miroslav Tsarev <mtzarev@gmail.com>
2024-08-25 22:05:32 +0300

    Update 3. Merge Arrays.js

--- 3. Merge Arrays.js
+++ 3. Merge Arrays.js
@@ -9,7 +9,7 @@ function mergeArrays(arr1, arr2) {
             newArr.push(num1.concat(num2));
         }
     }
-    console.log(newArr.join(` - `));
+    console.log(newArr.join(`  `));
 }
 mergeArrays(['5', '15', '23', '56', '35'],
     ['17', '22', '87', '36', '11']);
